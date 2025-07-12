8181e5239752ee88161c71847b41c5f0293abb40
FluffyGameDev <fluffygamedev@gmail.com>
2025-07-12 14:58:17 +0200

    Implemented demo in sandbox

--- sandbox/source/sandbox_module_entry_points.cpp
+++ sandbox/source/sandbox_module_entry_points.cpp
@@ -1,16 +1,145 @@
 #include "sandbox_module_entry_points.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <silk/engine/engine.h>
+#include <silk/engine/graphics/vertex_buffer.h>
 
 namespace silk::sandbox
 {
     void SandboxModuleEntryPoints::Install(Engine& engine)
     {
         engine.GetDebugToolbox().RegisterTool(&m_SandboxDebugTool);
+
+        m_Camera.SetPriority(1);
+        m_Camera.EnableScreenClear({});
+        m_Camera.ConfigurePerspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        m_Camera.LookAt(
+            glm::vec3(4, 3, 3),
+            glm::vec3(0, 0, 0),
+            glm::vec3(0, 1, 0)
+        );
+
+        m_DrawSlot.SetBoundFunction([this](GraphicsContext& context, const Camera& camera) { OnDraw(context, camera); });
+        m_Camera.GetDrawSignal().Connect(m_DrawSlot);
+        
+        m_UICamera.SetPriority(1);
+        m_UICamera.ConfigureOrthographic(0.0f, 800.0f, 0.0f, 600.0f);
+
+        m_DrawUISlot.SetBoundFunction([this](GraphicsContext& context, const Camera& camera) { OnDrawUI(context, camera); });
+        m_UICamera.GetDrawSignal().Connect(m_DrawUISlot);
+
+        GraphicsContext& graphicsContext{ engine.GetGraphicsContext() };
+        graphicsContext.EnableDepthTest(DepthTestFunction::Less);
+        graphicsContext.RegisterCamera(m_Camera);
+        graphicsContext.RegisterCamera(m_UICamera);
+
+        const char* vertexSource
+        {
+            "#version 330 core\n"
+            "\n"
+            "layout(location = 0) in vec3 vertexPosition_modelspace;\n"
+            "layout(location = 1) in vec3 vertexColor;\n"
+            "layout(location = 2) in vec2 vertexUV;\n"
+            "\n"
+            "uniform mat4 MVP;\n"
+            "\n"
+            "out vec3 o_vertexColor;\n"
+            "out vec2 UV;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "    gl_Position =  MVP * vec4(vertexPosition_modelspace, 1);\n"
+            "    o_vertexColor = vertexColor;\n"
+            "    UV = vertexUV;\n"
+            "}\n"
+        };
+
+        const char* fragmentSource
+        {
+            "#version 330 core\n"
+            "\n"
+            "in vec3 o_vertexColor;\n"
+            "in vec2 UV;\n"
+            "out vec3 color;\n"
+            "\n"
+            "uniform sampler2D myTextureSampler;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "    color = o_vertexColor * texture(myTextureSampler, UV).rgb;\n"
+            "}\n"
+        };
+
+        m_Shader.Create(vertexSource, fragmentSource);
+
+        static const float vertexData[] = {
+           -1.0f, -1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f,
+            1.0f, -1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f,
+            1.0f,  1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 0.0f,
+
+           -1.0f, -1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f,
+            1.0f,  1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 0.0f,
+           -1.0f,  1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    0.0f, 0.0f,
+        };
+
+        static const VertexDataType vertexDataTypes[]
+        {
+            VertexDataType::Float3, VertexDataType::Float3, VertexDataType::Float2
+        };
+
+        m_Mesh.Create();
+        m_Mesh.Bind();
+        m_Mesh.AddVertexBuffer(vertexData, sizeof(vertexData), vertexDataTypes, 3);
+        m_Mesh.Unbind();
+
+        static const unsigned char texData[]
+        {
+            255, 0, 0,   255,    0,   255, 0,   255,
+            0,   0, 255, 255,    255, 255, 255, 255,
+        };
+
+        m_Texture.Create(2, 2, texData);
     }
 
     void SandboxModuleEntryPoints::Uninstall(Engine& engine)
     {
+        m_Texture.Destroy();
+        m_Mesh.Destroy();
+        m_Shader.Destroy();
+
+        m_Camera.GetDrawSignal().Disconnect(m_DrawSlot);
+        engine.GetGraphicsContext().UnregisterCamera(m_Camera);
+
         engine.GetDebugToolbox().UnregisterTool(&m_SandboxDebugTool);
+    }
+
+    void SandboxModuleEntryPoints::OnDraw(GraphicsContext& context, const Camera& camera)
+    {
+        glm::mat4 Model = glm::mat4(1.0f);
+        glm::mat4 mvp = camera.GetProjectionMatrix() * camera .GetViewMatrix() * Model;
+
+        m_Shader.Bind();
+        m_Shader.SetUniform("MVP", mvp);
+        m_Texture.Bind();
+        context.DrawMesh(m_Mesh);
+        m_Texture.Unbind();
+        m_Shader.Unbind();
+    }
+
+    void SandboxModuleEntryPoints::OnDrawUI(GraphicsContext& context, const Camera& camera)
+    {
+        glm::mat4 Model = glm::mat4(1.0f);
+        Model = glm::translate(Model, glm::vec3{ 100.0f, 100.0f, 0.0f });
+        Model = glm::scale(Model, glm::vec3{ 100.0f });
+
+        glm::mat4 mvp = camera.GetProjectionMatrix() * camera .GetViewMatrix() * Model;
+
+        m_Shader.Bind();
+        m_Shader.SetUniform("MVP", mvp);
+        m_Texture.Bind();
+        context.DrawMesh(m_Mesh);
+        m_Texture.Unbind();
+        m_Shader.Unbind();
     }
 }
--- sandbox/source/sandbox_module_entry_points.h
+++ sandbox/source/sandbox_module_entry_points.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include <silk/core/slot.h>
 #include <silk/engine/module/module_entry_points.h>
 
+#include <silk/engine/graphics/camera.h>
+#include <silk/engine/graphics/mesh.h>
+#include <silk/engine/graphics/shader.h>
+#include <silk/engine/graphics/texture.h>
+
 #include "sandbox_debug_tool.h"
+
+namespace silk
+{
+    class GraphicsContext;
+}
 
 namespace silk::sandbox
 {
@@ -12,6 +23,17 @@ namespace silk::sandbox
         void Install(Engine& engine) override;
         void Uninstall(Engine& engine) override;
 
+        void OnDraw(GraphicsContext& context, const Camera& camera);
+        void OnDrawUI(GraphicsContext& context, const Camera& camera);
+
         SandboxDebugTool m_SandboxDebugTool;
+        Slot<void(GraphicsContext&, const Camera&)> m_DrawSlot;
+        Slot<void(GraphicsContext&, const Camera&)> m_DrawUISlot;
+
+        Camera m_Camera;
+        Camera m_UICamera;
+        Shader m_Shader;
+        Mesh m_Mesh;
+        Texture m_Texture;
     };
 }
